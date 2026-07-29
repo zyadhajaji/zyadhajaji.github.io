9dbba5e0752ccd0336a71970b3da94dcbeff6bf3
anakinzy <hajajizyad@gmail.com>
2026-07-29 20:16:26 +0100

    Add images array baseURL thumbnails and updateDisplayedImage

--- lab4/part2-imagegallery/main.js
+++ lab4/part2-imagegallery/main.js
@@ -1,6 +1,38 @@
+/*
+Name: Zyad Hajaji
+File: main.js
+Date: 29 July 2026
+Image gallery for Assignment 4 Part 2.
+Assignment note: skip task #8 (Enter key stretch goal).
+*/ 
 
 const displayedImage = document.querySelector(".displayed-img");
 const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+const images = [
+  { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+  { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+  { filename: "pic3.jpg", alt: "Purple and white pansies" },
+  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
+
+const baseURL =
+  "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+function updateDisplayedImage(e) {
+  displayedImage.src = e.target.src;
+  displayedImage.alt = e.target.alt;
+}
+
+for (const image of images) {
+  const newImage = document.createElement("img");
+  newImage.src = `${baseURL}${image.filename}`;
+  newImage.alt = image.alt;
+  newImage.tabIndex = "0";
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener("click", updateDisplayedImage);
+}
