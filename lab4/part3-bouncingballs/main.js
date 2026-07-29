c215affff4a0f7635aee900d01c9b57cb08b5475
anakinzy <hajajizyad@gmail.com>
2026-07-29 21:47:17 +0100

    Add animation loop function to draw and update balls
    Add animation loop function to draw and update balls

--- lab4/part3-bouncingballs/main.js
+++ lab4/part3-bouncingballs/main.js
@@ -70,5 +70,18 @@ while (balls.length < 25) {
     size
   );
   balls.push(ball);
+
+}
+
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+  }
+
+  requestAnimationFrame(loop);
   
 }
