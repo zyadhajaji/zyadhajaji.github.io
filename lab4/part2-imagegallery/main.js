972f2310b136d0cbec41e1f7d07ab510c4c91bfa
anakinzy <hajajizyad@gmail.com>
2026-07-29 20:05:42 +0100

    Add MDN image gallery starter: combine Play HTML+CSS into index.html and add starter main.js
    Add MDN image gallery starter: combine Play HTML+CSS into index.html and add starter main.js

---
+++ lab4/part2-imagegallery/main.js
@@ -0,0 +1,6 @@
+
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
