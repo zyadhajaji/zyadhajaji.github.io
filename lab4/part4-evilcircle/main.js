ca927510a98d2525ec02a14904d251c3aac4ece0
anakinzy <hajajizyad@gmail.com>
2026-07-31 16:54:42 +0100

    Add EvilCircle class constructor with WASD keyboard controls
    Add EvilCircle class constructor with WASD keyboard controls

--- lab4/part4-evilcircle/main.js
+++ lab4/part4-evilcircle/main.js
@@ -77,6 +77,31 @@ class Ball extends Shape {
   }
 }
 
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+}
+
 const balls = [];
 
 while (balls.length < 25) {
